c19df13b1a346f685bd403c248c705391b908257
Lulushu12 <radujosanu12@gmail.com>
2026-05-10 12:30:05 +0300

    remove icons, fix config

--- vite.config.js
+++ vite.config.js
@@ -16,9 +16,6 @@ export default defineConfig({
                                 background_color: '#13100d',
                                 display: 'standalone',
                                 orientation: 'portrait',
-                                icons: [
-                                  { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
-                                  { src: 'icon-512.png', sizes: '512x512', type: 'image/png' },
                                 ]
                               }
                             })
