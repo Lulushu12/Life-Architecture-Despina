75a4419740a81968d87c7c0ad68c3c31e1058fe6
Lulushu12 <radujosanu12@gmail.com>
2026-05-10 12:36:27 +0300

    drop pwa plugin, use manual manifest

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/Life-Architecture-Despina/',
-  plugins: [
-    react(),
-                            VitePWA({
-                              registerType: 'autoUpdate',
-                              manifest: {
-                                name: "Despina's Health System",
-                                short_name: 'Despina SHS',
-                                description: 'Sovereign Health System for Despina',
-                                theme_color: '#13100d',
-                                background_color: '#13100d',
-                                display: 'standalone',
-                                orientation: 'portrait',
-                              }
-                            })
-  ],
+  plugins: [react()],
 })
